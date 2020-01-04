de0cf7757f2610209f663c6d3525f18f7a8792e0
ekolog <ekolog055@gmail.com>
2020-01-04 16:03:33 +0100

    last commit

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ let minlimit = 0;
 let maxlimit = 100;
 let a = 2;
 let b = 3;
+let total = 0;
 
 for(minlimit; minlimit<maxlimit ; minlimit++){
     if((minlimit % a == 0) && (minlimit % b == 0)){
-        console.log(minlimit);
+        total+=minlimit
+        console.log(total);
     }
 }
