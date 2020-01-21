fb3b56880d4c3824fbae633fee998646e13c6d8c
ekolog <ekolog055@gmail.com>
2020-01-21 19:38:05 +0100

    last commit

---
+++ Farmer/main.js
@@ -0,0 +1,13 @@
+
+let total = 0;
+let inek = 4;
+let tavuk = 2;
+let domuz = 0;
+let koyun = 4;
+let ayaklar = [inek, inek, tavuk, domuz, inek];
+
+for(let i = 0;i<ayaklar.length;i++){
+    total += ayaklar[i];
+    
+}
+alert(total);
