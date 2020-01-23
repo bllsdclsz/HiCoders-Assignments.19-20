23e88f74d92705c8cae1d8ecde99a2546aeda034
ekolog <ekolog055@gmail.com>
2020-01-23 18:09:25 +0100

    last commit

--- Farmer/farmer.js
+++ Farmer/farmer.js
@@ -1,18 +1,19 @@
 let ayaklar = ["inek", "inek", "tavuk", "domuz", "inek"]
 let total = 0;
+let x = 0;
 
 for(let i = 0; i < ayaklar.length; i++){
     if(ayaklar[i]==="inek"){
-        ayaklar[i]==4;
+        x=4;
     }else if(ayaklar[i]==="tavuk"){
-        ayaklar[i]==2;
+        x=2;
     }else if(ayaklar[i]==="koyun"){
-        ayaklar[i]==4;
+        x=4;
     }else if(ayaklar[i]==="domuz"){
         continue;
     }
     
-    total += i;
+    total += x;
 }
 
 console.log(total);
