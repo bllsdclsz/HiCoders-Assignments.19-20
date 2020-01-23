cac6d06c69cc1c7687bf74f187704b8e4cc8d301
ekolog <ekolog055@gmail.com>
2020-01-23 16:47:22 +0100

    last commit

--- Arrays/main.js
+++ Arrays/main.js
@@ -1,34 +1,16 @@
 let A = [1, "d", 999, "js", 2020, 564, "arrays"];
 let B = [999, "js", 09, "s", 2020, "link", "arrays", 9076];
-let AB = 0;
+let AB = [];
 
-for(let i = 0;i<(A.length&&B.length);i++){
-    if((A==true)&&(B==true)){
-        AB+=A+B;
+for(let i = 0;i<A.length;i++){
+    for(let j = 0;j<B.length;j++){
+        if(A[i]===B[j]){
+            AB += A[i];
+        }else if(!A[i]===B[j]){
+            i++;
+        }
     }
 }
+
 console.log(AB);
-
-
-let total = 0;
-let inek = 4;
-let tavuk = 2;
-let domuz = 0;
-let koyun = 4;
-let ayaklar = ["inek", "inek", "tavuk", "domuz", "inek"];
-
-for(let i = 0;i<ayaklar.length;i++){
-    if(i == "inek"){
-        ayaklar[i] == 4;
-    }else if(i == "koyun"){
-        ayaklar[i] == 4;
-    }else if(i == "tavuk"){
-        ayaklar == 2;
-    }else if(i == "domuz"){
-        ayaklar[i] == 0;
-    }
-    total += i;
-    
-}
-//alert(total);
-console.log(total);
+alert(AB);
