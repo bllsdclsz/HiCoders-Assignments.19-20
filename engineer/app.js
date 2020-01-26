cb4628616de936de324524567a17cd4e8791cfe1
ekolog <ekolog055@gmail.com>
2020-01-26 13:56:13 +0100

    last commit

---
+++ engineer/app.js
@@ -0,0 +1,23 @@
+let circle_a = circle_area(4);
+alert("area of circle");
+alert(circle_a);
+
+let rectangle_a = rectangle_area(3, 5);
+alert("area of rectangle");
+alert(rectangle_a);
+
+let circle_c = circle_circle (8);
+alert("circle of circle");
+alert(circle_c);
+
+let rectangle_p = rectangle_perimeter(10, 9);
+alert("perimeter of rectangle");
+alert(rectangle_p);
+
+let cube_v = cube_volume(13);
+alert("volume of cube");
+alert(cube_v);
+
+let cube_a = cube_area(13);
+alert("sum of the lateral areas of the cube");
+alert(cube_a);
---
+++ engineer/calculation_methods.js
@@ -0,0 +1,29 @@
+function circle_area (r){
+    let x = 2*(Math.PI)*(Math.pow(r, 2));
+    return x;
+}
+
+function rectangle_area (a, b){
+    let y = 2*a*b;
+    return y; 
+}
+
+function circle_circle (r) {
+    let z = 2*(Math.PI)*r;
+    return z;
+}
+
+function rectangle_perimeter (a, b){
+    let u = 2*a*b;
+    return u;
+}
+
+function cube_volume (a){
+    let t = Math.pow(a, 3);
+    return t;
+}
+
+function cube_area (a){
+    let v = 6*(Math.pow(a, 2));
+    return v;
+}
