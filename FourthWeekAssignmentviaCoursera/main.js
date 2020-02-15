984af7a8f5d9ee3209a247b7daba8c4c7065420b
ekolog <ekolog055@gmail.com>
2020-02-15 19:54:48 +0100

    last commit by Coursera

---
+++ FourthWeekAssignmentviaCoursera/main.js
@@ -0,0 +1,8 @@
+function billingFunction (){
+    if(document.getElementById('same').checked){
+        document.getElementById("billingName").value=document.getElementById("shippingName").value;
+        document.getElementById("billingZip").value=document.getElementById("shippingZip").value;
+    }else{
+        return false;
+    }
+}
