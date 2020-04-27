d3c0b1eda2d0e03be1da4bbf1b3703c024a7cbc4
ekolog <ekolog055@gmail.com>
2020-04-27 18:55:35 +0200

    last commit

---
+++ praxis/app.js
@@ -0,0 +1,12 @@
+/**
+ * Akis
+ * -Sistem rastgele evcil hayvanlari sahipleriyle beraber bir siraya koyar.
+ * -Sirasi gelen uygun oldugu doktora gider.
+ * -Bilgiler gün sonunda ekrana yazdirilir.
+ * 
+ * Analiz Kismi
+ * ---Model objeleri: Evcil haynalar, Sahipler
+ * ---View objeleri: sonuc ekrani
+ * 
+ * -
+ */
