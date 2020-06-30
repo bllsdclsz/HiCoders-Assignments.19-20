7bdf0e91c7df38a3caaab91d907f3e9e0a86bf6c
ekolog <ekolog055@gmail.com>
2020-06-30 16:55:05 +0200

    last commit

---
+++ React-training/data.js
@@ -0,0 +1,15 @@
+const DATA = [{
+    "name":"Teddy Bär A",
+    "price":"Fr.25",
+    "avatar":"abc"
+},
+{
+    "name":"Teddy Bär B",
+    "price":"Fr.40",
+    "avatar":"def"
+},
+{
+    "name":"Teddy Bär C",
+    "price":"Fr.60",
+    "avatar":"ghi"
+}]
